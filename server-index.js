564529caaa950edd1f8c466c8f26be07d93c55a9
Aedla Varshit <varshitreddy27914@gmail.com>
2025-08-28 15:16:59 +0530

    Update server-index.js

--- server-index.js
+++ server-index.js
@@ -1,44 +1,34 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname, ".")));
-
-let peers = {};
+app.use(express.static("public"));
 
 io.on("connection", (socket) => {
-  console.log("🔗 User connected:", socket.id);
+  console.log("User connected:", socket.id);
 
-  peers[socket.id] = socket.id;
+  socket.broadcast.emit("new-peer", socket.id);
 
-  socket.broadcast.emit("user-joined", socket.id);
-
-  socket.on("offer", (data) => {
-    io.to(data.to).emit("offer", { from: socket.id, sdp: data.sdp });
+  socket.on("peer-offer", ({ to, offer }) => {
+    io.to(to).emit("peer-offer", { from: socket.id, offer });
   });
 
-  socket.on("answer", (data) => {
-    io.to(data.to).emit("answer", { from: socket.id, sdp: data.sdp });
+  socket.on("peer-answer", ({ to, answer }) => {
+    io.to(to).emit("peer-answer", { from: socket.id, answer });
   });
 
-  socket.on("ice-candidate", (data) => {
-    io.to(data.to).emit("ice-candidate", { from: socket.id, candidate: data.candidate });
+  socket.on("peer-ice", ({ to, candidate }) => {
+    io.to(to).emit("peer-ice", { from: socket.id, candidate });
   });
 
   socket.on("disconnect", () => {
-    console.log("❌ User disconnected:", socket.id);
-    delete peers[socket.id];
-    socket.broadcast.emit("user-left", socket.id);
+    console.log("User disconnected:", socket.id);
   });
 });
 
-server.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on ${PORT}`));
